944f5c33155e4950044e9da1a1b42ae8aa169daf
Gabriel Galperin <galperin@alunos.utfpr.edu.br>
2021-05-25 00:47:13 -0300

    Models created and saved

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 # load packages
 library(caret)
+library(neuralnet)
 
 # load csv database
 Volumes <- read.csv2("http://www.razer.net.br/datasets/Volumes.csv")
@@ -13,3 +14,15 @@ index <- createDataPartition(Volumes$VOL, p=0.80, list = FALSE)
 
 volume_train <- Volumes[index, ]
 volume_test <- Volumes[-index, ]
+
+# train random Forest model
+rf <- train(VOL~., data = volume_train, method = "rf") # train model
+saveRDS(rf, "rf.rds") # save trained model
+
+# train SVM model
+svm <- train(VOL~., data = volume_train, method = "svmRadial")
+saveRDS(svm, "svm.rds") #save trained model
+
+# train neural networks
+nn <- train(VOL~., data = volume_train, method = "neuralnet")
+saveRDS(nn, "neuralnet.rds") #save trained model
